21c4426083560c5250b5bf6ed617826e7bbc1aa6
Azir@9200 <aziruddin83@hotmail.com>
2023-08-02 17:47:30 +0100

    updated files

---
+++ variable.js
@@ -0,0 +1,41 @@
+var harryGot = 1000;
+var fruitCost = 700;
+var returnMom = harryGot - fruitCost;
+//console.log(returnMom);
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50
+var total = math + biology + chemistry + physics + bangla;
+//console.log(total);
+
+var line1 = "I am going to be";
+var line2 = "an awesome web developer.";
+var line3 = line1 + " " + line2;
+//console.log(line3);
+const sarahGot = 119;
+const momsNumber = 5;
+const devide = sarahGot / momsNumber;
+Jconsole.log(devide);
+
+
+var ammu_dise = 1000;
+var priceApple = 400;
+var priceOrange = 300;
+var totalCost = priceApple + priceOrange;
+var priceRemain = ammu_dise - totalCost;
+//console.log(totalCost);
+//console.log(priceRemain);
+
+
+var numbers = [29, 38, 40, 45, 55,];
+
+console.log(numbers[3]);
+
+
+
+
+
+
+
